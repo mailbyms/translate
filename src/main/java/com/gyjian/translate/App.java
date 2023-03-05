b7c2ac2b6fa696e806913c36d7d26eed54075bd9
mike <mike@cloudcall.hk>
2023-03-05 22:37:47 +0800

    规范代码写法。TODO：使用restemplate替换HttpGet类

--- src/main/java/com/gyjian/translate/App.java
+++ src/main/java/com/gyjian/translate/App.java
@@ -1,8 +1,10 @@
 package com.gyjian.translate;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,122 +18,118 @@ import com.alibaba.fastjson.JSONObject;
 
 import com.baidu.translate.demo.TransApi;
 
+@Slf4j
 @SpringBootApplication
 public class App {
-	// 在平台申请的APP_ID 详见
-	// http://api.fanyi.baidu.com/api/trans/product/desktop?req=developer
-	private static final String APP_ID = "20190709000316278";
-	private static final String SECURITY_KEY = "XODK2pODq31WwXHujurS";
+    // 在平台申请的APP_ID 详见
+    // http://api.fanyi.baidu.com/api/trans/product/desktop?req=developer
+    private static final String APP_ID = "20190709000316278";
+    private static final String SECURITY_KEY = "XODK2pODq31WwXHujurS";
 
-	public static void main(String[] args) {
-		if (args.length != 1) {
-			System.out.println("Usage:java -jar translate.jar xxx.srt");
-			return;
-		}
-		String inFile = args[0]; //"/Users/jane/Downloads/a.srt";
-		System.out.println("input srt file:" + inFile);
-		
-		String outFile = inFile.replaceAll("\\.srt", "\\.baidu.srt");
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            log.info("Usage:java -jar translate.jar xxx.srt");
+            //return;
+        }
+        String inFile = args[0];
+        //String inFile = "C:\\Users\\YOGA\\Downloads\\Mayor.of.Kingstown.S02E07.Drones.1080p.AMZN.WEB-DL.DDP5.1.H.264-NTb.srt";
 
-		FileOutputStream outSTr = null;
-		BufferedOutputStream Buff = null;
+        log.info("input srt file:{}", inFile);
 
-		try {
-			InputStream is = new FileInputStream(inFile);
-			BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        int dotIndex = inFile.lastIndexOf('.'); // 获取最后一个点的位置
+        String nameWithoutExt = inFile.substring(0, dotIndex); // 获取没有后缀的文件名
 
-			outSTr = new FileOutputStream(new File(outFile));
-			Buff = new BufferedOutputStream(outSTr);
-			String reg = "^([0-9])*:([0-9])*"; // 用来匹配时间轴格式，如 00:00:14,000 --> 00:00:17,760
-			Pattern pattern = Pattern.compile(reg);
-			
-			String reg2 = "<[^>]+>";		// 用来匹配某些 srt如下xml格式，<font color="#ffffff">lays the foundation</font>
-			Pattern pattern2 = Pattern.compile(reg2);
+        String outFile = nameWithoutExt + ".baidu.srt";
+        try (
+                InputStream is = new FileInputStream(inFile);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                FileOutputStream outSTr = new FileOutputStream(outFile);
+                BufferedOutputStream Buff = new BufferedOutputStream(outSTr)
+        ) {
+            String reg = "^([0-9])*:([0-9])*"; // 用来匹配时间轴格式，如 00:00:14,000 --> 00:00:17,760
+            Pattern pattern = Pattern.compile(reg);
 
-			String lyric = "";
-			String str = null;
-			while (true) {
-				str = reader.readLine();
-				if (str != null) {
-					Matcher matcher2 = pattern2.matcher(str);
-					if (matcher2.find()) {
-						str = matcher2.replaceAll("");	// 去掉 xml 格式 <>
-					}
-					
-					System.out.println(str);
+            String reg2 = "<[^>]+>";        // 用来匹配某些 srt如下xml格式，<font color="#ffffff">lays the foundation</font>
+            Pattern pattern2 = Pattern.compile(reg2);
 
-					Matcher matcher = pattern.matcher(str);
+            String lyric = "";
+            String str;
+            while (true) {
+                str = reader.readLine();
+                if (str != null) {
+                    Matcher matcher2 = pattern2.matcher(str);
+                    if (matcher2.find()) {
+                        str = matcher2.replaceAll("");    // 去掉 xml 格式 <>
+                    }
 
-					if ((str.length() < 5) || matcher.find()) {
-						// 如果有积聚英文原句，刚翻译并写到文件里
-						if (lyric.length() > 0) {
-							try {
-								TransApi api = new TransApi(APP_ID, SECURITY_KEY);
+                    log.info(str);
 
-								String query = lyric;
-								query = (api.getTransResult(query, "en", "zh"));
-								
-								JSONObject jo = JSONObject.parseObject(query);
-								JSONArray jArray = jo.getJSONArray("trans_result");
-								if(jArray == null) {
-									System.out.println("翻译失败，接口结果为：" + query);
-									int errno = jo.getIntValue("error_code");
-									if (errno == 54003) {
-										
-									}
-								}
-								
-								JSONObject jo2 = (JSONObject) jArray.get(0);
+                    Matcher matcher = pattern.matcher(str);
 
-								// Convert from Unicode to UTF-8
-								String string = jo2.getString("dst");
-								byte[] utf8 = string.getBytes("UTF-8");
-								// Convert from UTF-8 to Unicode
-								string = new String(utf8, "UTF-8");
+                    if ((str.length() < 5) || matcher.find()) {
+                        // 如果有积聚英文原句，刚翻译并写到文件里
+                        if (lyric.length() > 0) {
+                            try {
+                                TransApi api = new TransApi(APP_ID, SECURITY_KEY);
 
-								System.out.println(string);
-								Buff.write(utf8);
-								Buff.write('\n');
-								Buff.write(lyric.getBytes());
-								Buff.write('\n');
+                                String query = lyric;
+                                query = (api.getTransResult(query, "en", "zh"));
 
-								Buff.flush();
-								
-								Thread.sleep(1000);
-								
-							} catch (Exception e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-								break;
-							}
-							
-							lyric = "";			// 翻译完，复位到空串
-						}
-												
-						
-						// 把原文写到文件
-						Buff.write(str.getBytes());
-						Buff.write('\n');
-						Buff.flush();
-					} else {						// 读到一句英文句子，先存起来
-						lyric += " " + str;
-						lyric = lyric.trim();
-					}
-				}
+                                JSONObject jo = JSONObject.parseObject(query);
+                                JSONArray jArray = jo.getJSONArray("trans_result");
+                                if (jArray == null) {
+                                    log.warn("翻译失败，接口结果为：{}", query);
+                                    int errno = jo.getIntValue("error_code");
+                                    if (errno == 54003) {
 
-				else {
-					break;
-				}
-			}
+                                    }
+                                }
 
-			is.close();
+                                JSONObject jo2 = (JSONObject) jArray.get(0);
 
-			Buff.close();
-			outSTr.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+                                // Convert from Unicode to UTF-8
+                                String string = jo2.getString("dst");
+                                byte[] utf8 = string.getBytes(StandardCharsets.UTF_8);
+                                // Convert from UTF-8 to Unicode
+                                string = new String(utf8, StandardCharsets.UTF_8);
 
-		System.out.println("完成");
-	}
+                                log.info(string);
+                                Buff.write(utf8);
+                                Buff.write('\n');
+                                Buff.write(lyric.getBytes());
+                                Buff.write('\n');
+
+                                Buff.flush();
+
+                                Thread.sleep(20);
+
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                log.error("Exception", e);
+                                break;
+                            }
+
+                            lyric = "";            // 翻译完，复位到空串
+                        }
+
+                        // 把原文写到文件
+                        Buff.write(str.getBytes());
+                        Buff.write('\n');
+                        Buff.flush();
+                    } else {                        // 读到一句英文句子，先存起来
+                        lyric += " " + str;
+                        lyric = lyric.trim();
+                    }
+                } else {
+                    break;
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("Exception", e);
+        }
+
+        log.info("翻译完成");
+    }
 }
